5b1682222e780f67e8ea78b8446203e3169007ed
Alejandro Andres <fuzzy.alej@gmail.com>
2014-03-20 20:04:59 +0100

    added some more database operations

--- cds.lisp
+++ cds.lisp
@@ -13,6 +13,20 @@
   (read-line *query-io*))
 
 
+;; Persistence
+(defun save-db (filename)
+  (with-open-file (out filename
+                       :direction :output
+                       :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+
 ;; Model
 (defun make-cd (title artist rating ripped)
   (list :title title
@@ -35,3 +49,41 @@
 (defun add-cds ()
   (loop (add-cd (interactive-add-cd))
         (if (not (y-or-n-p "Another?: ")) (return))))
+
+;; Queries
+(defun select-by-artist (artist)
+  "Bad because we will have several functions all the same"
+  (remove-if-not
+    #'(lambda (cd) (equal (getf cd :artist) artist))
+    *db*))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun artist-selector (artist)
+  "Bad too, because we will have several functions all the same!"
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  "First version of where, cheeky"
+  #'(lambda (cd)
+      (and
+        (if title (equal (getf cd :title) title) t)
+        (if artist (equal (getf cd :artist) artist) t)
+        (if rating (equal (getf cd :rating) rating) t)
+        (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  "First version of update"
+  (setf *db*
+        (mapcar
+          #'(lambda (cd)
+              (when (funcall selector-fn cd)
+                (if title (setf (getf cd :title) title))
+                (if artist (setf (getf cd :artist) artist))
+                (if rating (setf (getf cd :rating) rating))
+                (if ripped-p (setf (getf cd :ripped) ripped)))
+              cd) *db*)))
+
+(defun delete (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
