1a79f1ec6bbf8643a77707a3a52e5475642f2a9b
Alejandro Andres <fuzzy.alej@gmail.com>
2014-03-20 20:16:26 +0100

    added where macro

--- cds.lisp
+++ cds.lisp
@@ -64,16 +64,16 @@
   "Bad too, because we will have several functions all the same!"
   #'(lambda (cd) (equal (getf cd :artist) artist)))
 
-(defun where (&key title artist rating (ripped nil ripped-p))
-  "First version of where, cheeky"
-  #'(lambda (cd)
-      (and
-        (if title (equal (getf cd :title) title) t)
-        (if artist (equal (getf cd :artist) artist) t)
-        (if rating (equal (getf cd :rating) rating) t)
-        (if ripped-p (equal (getf cd :ripped) ripped) t))))
+;(defun where (&key title artist rating (ripped nil ripped-p))
+;  "First version of where, cheeky"
+;  #'(lambda (cd)
+;      (and
+;        (if title (equal (getf cd :title) title) t)
+;        (if artist (equal (getf cd :artist) artist) t)
+;        (if rating (equal (getf cd :rating) rating) t)
+;        (if ripped-p (equal (getf cd :ripped) ripped) t))))
 
-(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+(defun update-cd (selector-fn &key title artist rating (ripped nil ripped-p))
   "First version of update"
   (setf *db*
         (mapcar
@@ -85,5 +85,17 @@
                 (if ripped-p (setf (getf cd :ripped) ripped)))
               cd) *db*)))
 
-(defun delete (selector-fn)
+(defun delete-cd (selector-fn)
   (setf *db* (remove-if selector-fn *db*)))
+
+
+;; Macros
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+        collect (make-comparison-expr (pop fields) (pop fields))))
+
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
